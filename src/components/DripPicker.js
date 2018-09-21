838b7f279d34176edd85ad1c0560a30ef73e2272
midimurphdesigns <midimurphdesigns@gmail.com>
2018-09-21 13:53:02 -0700

    added scaffold for drip picker

---
+++ src/components/DripPicker.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import './DripPicker.css'
+
+export default class DripPicker extends Component {
+  render() {
+    return (
+      <div id="drip-picker">
+        <h1>Drip Picker</h1>
+      </div>
+    )
+  }
+}
--- src/components/DripSuggestion.js
+++ src/components/DripSuggestion.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react'
-import './DripSuggestion.css'
+import React, { Component } from "react";
+
+import DripPicker from "./DripPicker";
+import "./DripSuggestion.css";
 
 export default class DripSuggestion extends Component {
   render() {
@@ -7,7 +9,8 @@ export default class DripSuggestion extends Component {
       <div id="drips">
         <h3>Drip Suggestions</h3>
         <h4>Follow these directions to figure out which drip to use.</h4>
+        <DripPicker />
       </div>
-    )
+    );
   }
 }
