235908cd1f15647772c7300484ce21dc54fe9ae2
midimurphdesigns <midimurphdesigns@gmail.com>
2018-09-23 11:21:03 -0700

    added assets and minor changes

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -7,11 +7,16 @@ export default class IntroSection extends Component {
   render() {
     return (
       <div id="intro-container">
-        <div>
+        <div className="top-header">
           <h1>Welcome to Drip!</h1>
+          <h3>A Pediatrics Utility App</h3>
           <h2>Figure out which drip to use in a heart beat</h2>
         </div>
         <div className="overview">
+          {/* <div className="overview-heading">
+            
+          </div> */}
+          <div className="overview-icon-section">
           <div className="icon">
             <FontAwesomeIcon className="overview-icon" icon="mobile-alt" />
             <h3>Open Drip Picker App</h3>
@@ -29,6 +34,7 @@ export default class IntroSection extends Component {
             <FontAwesomeIcon className="overview-icon" icon="tint" />
             <h3>Administor the Correct Drip</h3>
           </div>
+        </div>
         </div>
         <div className="arrow-down-container">
           <a className="arrow-down" href="#drips">
