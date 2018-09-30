37e064cff5548ba996f873a54df46d7fcf12a2dd
Rahul Vaidya <rvaidyaa@gmail.com>
2018-09-30 14:31:07 -0700

    added axios

--- src/components/FormulaWizard.js
+++ src/components/FormulaWizard.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react'
-
+import axios from "axios";
 import './FormulaWizard.css'
 
 export default class FormulaWizard extends Component {
+  state = {
+    enteralformulas: []
+  };
+  componentDidMount = () => {
+    axios
+      .get("https://sheetlabs.com/ENT/EnteralUpdated")
+      .then(function(response) {
+        
+        console.log(response.data);
+        this.storeFormulas(response.data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+  storeFormulas = data => {
+    data.map( formula => {
+      console.log(formula);
+      return(formula);
+    })
+  }
   render() {
     return (
       <div id="formula-wizard">
